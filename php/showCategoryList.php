7cde19eefef3d29a9d26bdca119e2cc2c45e506a
milad khanmohammadi <milad.khanmohammadi@gmail.com>
2015-07-18 23:08:16 +0330

    server side code

--- php/showCategoryList.php
+++ php/showCategoryList.php
@@ -4,6 +4,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header('Content-Type: application/json');
+//header('Content-Type: text/html; charset=utf-8');
 require_once("../wp-load.php");
 $args = array(
 	'type'                     => 'post',
@@ -20,10 +21,13 @@ $args = array(
 	'pad_counts'               => false 
 ); 
 $categories = get_categories($args);
+$cat = new stdClass();
+$cat->term_id='all';
+$cat->name='همه مقالات';
+$cat->cat_ID='all';
+array_unshift($categories,$cat);
 foreach($categories as $key=>$value)
 {
-  $classNumber ++;
-  $value->classNumber = $classNumber;
   $args = array(
 	'posts_per_page'   => 1,
 	'offset'           => 0,
@@ -47,30 +51,63 @@ foreach($categories as $key=>$value)
   $thumb_url = wp_get_attachment_image_src($post_thumbnail_id,'small', true);
   $value->thumbnail=$thumb_url[0];
   $value->post=$post;
+  switch ($value->cat_ID) {    
+    case '41':
+        $value->description = 'راه های افزایش امید و انگیزش در زندگی و کار';
+        break;
+    case '47':
+        $value->description = 'آیا می دانید بهترین راه های سپری کردن اوقات فراغت چیست';
+        break;  
+    case '28':
+        $value->description = 'افزایش انگیزه و راندمان کاری و منع آفت روانی';
+        break;
+    case '1504':
+        $value->description = 'تجملات مد فشن و زندگی با ابزار مدرن';
+        break;
+    case '44':
+        $value->description = 'اگر به دنبال اطلاعاتی در مورد تناسب اندام احتیاج دارید این قسمت را از دست ندهید';
+        break;
+    case '29':
+        $value->description = 'آخرین اخبار تکنولوژی و گجت های روز';
+        break;
+    case '43':
+        $value->description = 'هر آنچه در مورد مدیریت خانواده احتیاج دارید';
+        break;
+    case '75':
+        $value->description = 'تکنیک هایی جهت چیدمان جذاب تر محیط خانه و کار';
+        break; 
+    case '42':
+        $value->description = 'بهبود روابط کاری اجتماعی را در این قسمت بخوانید';
+        break; 
+    case '26':
+        $value->description = 'مقالاتی جهت تغییر و بهبود سبک زندگی مان';
+        break; 
+    case '46':
+        $value->description = 'دستورالعمل هایی جهت افزایش ایمنی بدن وسلامتی';
+        break; 
+    case '45':
+        $value->description = 'فواید انواع مختلف رژیم های غذایی و نوشیدنی های مختلف';
+        break;
+    case '292':
+        $value->description = 'چگونه شرکت و زیر دستانمان را به بهترین شکل راهبری کنیم';
+        break;
+    case '27':
+        $value->description = 'هر آنچه در مورد پول لازم است بدانید';
+        break;
+    case '30':
+        $value->description = 'هر آنچه لازم است در مورد کارتان وحواشی آن بدانید';
+        break;
+    case '1':
+        $value->description = 'روش های کارآفرینی را اینجا بیاموزید';
+        break;   
+    case '1503':
+        $value->description = 'اطلاعاتی جذاب در مورد نقاط دیدنی دنیا';
+        break;
+    case '5598':
+        $value->description = 'مقالات جذابی که به نظر ما ارزش خواندن دارند';
+        break;
+    default:
+        $value->description = 'لیست تمامی مقالات به ترتیب جدیدترین ها';
+  }
 }
-$kol[0] = $categories;
-// get last post
-$args = array(
-	'posts_per_page'   => 1,
-	'offset'           => 0,
-	'category'         => '',
-	'category_name'    => '',
-	'orderby'          => 'ID',
-	'order'            => 'DESC',
-	'include'          => '',
-	'exclude'          => '',
-	'meta_key'         => '',
-	'meta_value'       => '',
-	'post_type'        => 'post',
-	'post_mime_type'   => '',
-	'post_parent'      => '',
-	'post_status'      => 'publish',
-	'suppress_filters' => true 
-  );
-
-  $post = get_posts($args);
-  $post_thumbnail_id = get_post_thumbnail_id($post[0]->ID);
-  $thumb_url = wp_get_attachment_image_src($post_thumbnail_id,'small', true);
-//
-$kol[1] = $thumb_url[0];
-echo (json_encode($kol));
+echo (json_encode($categories));
